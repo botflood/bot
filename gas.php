2983f5c26aae15e3cfd92be85d38cd417e4e464a
botflood <67453254+botflood@users.noreply.github.com>
2020-06-27 15:13:54 +0700

    Update gas.php

--- gas.php
+++ gas.php
@@ -53,7 +53,7 @@ echo "\e[1m\e[95m▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬
         echo color("yellow",".");
         sleep(10);
         }
-        $code1 = request('/go-promotions/v1/promotions/enrollments', $token, '{"promo_code":"EATLAH"}');
+        $code1 = request('/go-promotions/v1/promotions/enrollments', $token, '{"promo_code":"EBADAHMAKAN"}');
         $message = fetch_value($code1,'"message":"','"');
         if(strpos($code1, 'Promo kamu sudah bisa dipakai')){
         echo "\n".color("green","".$message);
