22d34e27c1a4df4fd6d2e1ff2a2baf514c4917eb
botflood <67453254+botflood@users.noreply.github.com>
2020-06-30 16:57:14 +0000

    Update function.php

--- function.php
+++ function.php
@@ -11,7 +11,7 @@ $header[] = "X-AppVersion: 3.30.2";
 $header[] = "X-UniqueId: ".time()."57".mt_rand(1000,9999);
 $header[] = "Connection: keep-alive";
 $header[] = "X-User-Locale: id_ID";
-$header[] = "X-Location: -6.9151514,107.6189794";
+$header[] = "X-Location: -6.2143058,106.8432248";
 $header[] = "X-Location-Accuracy: 3.0";
 if ($pin):
 $header[] = "pin: $pin";
